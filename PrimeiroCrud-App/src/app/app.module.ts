76432c3056ae5bc01f5102a27da55c79ecdff918
Ricardo Tenorio <ricardobatenorio@gmail.com>
2021-01-22 01:28:51 -0300

    Adding Angular Project

---
+++ PrimeiroCrud-App/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BandasComponent } from './bandas/bandas.component';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+      BandasComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ PrimeiroCrud-App/src/app/bandas/bandas.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bandas',
+  templateUrl: './bandas.component.html',
+  styleUrls: ['./bandas.component.scss']
+})
+export class BandasComponent implements OnInit {
+
+  bandas: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getBandas();
+  }
+
+  getBandas(){
+    this.http.get('http://localhost:5000/api/values').subscribe(
+      response => { this.bandas = response; 
+      }, error => {
+        console.log(error);
+      }
+    );
+  }
+
+}
